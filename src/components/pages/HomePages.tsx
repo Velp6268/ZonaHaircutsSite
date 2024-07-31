95c6fd405af27c4abbc03bd5d4af3cf949cba191
Velp <90183727+Velp6268@users.noreply.github.com>
2024-07-31 19:21:59 +0300

    v0.5

--- src/components/pages/HomePages.tsx
+++ src/components/pages/HomePages.tsx
@@ -1,21 +1,23 @@
 import React from 'react';
 import MainBlock from "../sections/MainBlock/MainBlock.tsx";
-import Header from "../sections/Header/Header.tsx";
+
 import WhatWeDo from "../sections/WhatWeDo/WhatWeDo.tsx";
-import Contact from '../sections/Contact/Contact.tsx';
-import OurTeam from '../sections/OurTeam/OurTeam.tsx';
-import OurWorks from '../sections/OurWorks/OurWorks.tsx';
+import Services from "../sections/Services/Services.tsx";
+import OurTeam from "../sections/OurTeam/OurTeam.tsx";
+import Contact from "../sections/Contact/Contact.tsx";
+import OurWorks from "../sections/OurWorks/OurWorks.tsx";
+
 
 const HomePages : React.FC = ()  => {
     return (
         <div>
-            <Header/>
             <MainBlock/>
             <WhatWeDo/>
-            
+            <Services/>
             <OurTeam/>
             <OurWorks/>
             <Contact/>
+
         </div>
     );
 };
